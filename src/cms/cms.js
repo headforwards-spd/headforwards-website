f2fb934da4c8b59d0a980e680f194a14476d98ec
Andy Weir <andy.weir@headforwards.com>
2019-10-07 17:51:07 +0100

    .

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,12 +5,12 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 // import AboutPagePreview from './preview-templates/AboutPagePreview'
 // import BlogPostPreview from './preview-templates/BlogPostPreview'
 // import ProductPagePreview from './preview-templates/ProductPagePreview'
-import DesignPagesPreview from './preview-templates/design-pages'
+import DesignPagePreview from './preview-templates/design-page'
 //
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 //
-CMS.registerPreviewTemplate('design-pages', DesignPagesPreview)
+CMS.registerPreviewTemplate('design-pages', DesignPagePreview)
 // CMS.registerPreviewTemplate('about', AboutPagePreview)
 // CMS.registerPreviewTemplate('products', ProductPagePreview)
 // CMS.registerPreviewTemplate('blog', BlogPostPreview)
