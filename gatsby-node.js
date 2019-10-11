7c696fea75632c7e64f14d027521970ea1dcf428
Andy Weir <andy.weir@headforwards.com>
2019-10-11 13:08:41 +0100

    WIP

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,57 @@
-const _ = require('lodash')
+const { createFilePath } = require('gatsby-source-filesystem');
+const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+
 const path = require('path')
 
-// exports.createPages = ({ actions, graphql }) => {
-//   const { createPage } = actions
+exports.createPages = ({ actions, graphql }) => {
+
+    const { createPage } = actions;
+
+    return getData(graphql)
+        .then(({ data, errors }) => (!errors ? Promise.resolve(data) : Promise.reject(errors)))
+        .then(({ designPages }) => createAllPosts(createPage, designPages));
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+    const { createNodeField } = actions;
+    const { internal = null } = node;
+    const { type } = internal;
+    fmImagesToRelative(node); // convert image paths for gatsby images
+
+    if (type === `MarkdownRemark`) {
+        const value = createFilePath({ node, getNode });
+        return createNodeField({
+                                   name: `slug`,
+                                   node,
+                                   value,
+                               });
+    }
+    return Promise.resolve();
+};
+
+function createAllPosts(createPage, { edges: posts = [] }) {
+    // const firstIndex = 0;
+    // const lastIndex = posts.length - 1;
+
+    return Promise.all(
+        posts.map(({ node: post }, index) => {
+
+            const { id, fields, frontmatter } = post;
+            const { slug } = fields;
+            const { path: postPath, type } = frontmatter;
+
+            // const { id: prevId = '' } = index > firstIndex ? posts[index - 1].node : {};
+            // const { id: nextId = '' } = index < lastIndex ? posts[index + 1].node : {};
+
+            return createPage({
+                                  path: slug,
+                                  component: path.resolve(`src/templates/${postPath}/${type}-page.js`),
+                                  context: { id }//, prevId, nextId },
+                              });
+        })
+    );
+}
+
 //
 //   return graphql(`
 //     {
@@ -83,3 +132,24 @@ const path = require('path')
 //     })
 //   }
 // }
+
+function getData(graphql) {
+    return graphql(`
+        {
+            designPages: allMarkdownRemark {
+                edges {
+                    node {
+                        id
+                        fields {
+                            slug
+                        }
+                        frontmatter {
+                            type
+                            path
+                        }
+                    }
+                }
+            }
+        }
+    `);
+}
