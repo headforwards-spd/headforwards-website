0cfaff8bd7512ee38887a55b83f8b770628a480c
Emma <emma.cheney@headforwards.com>
2019-10-11 10:30:13 +0100

    change toml file

--- src/components/article-block/article-block-component.js
+++ src/components/article-block/article-block-component.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {flexRow} from './article-block-component.module.scss';
 
-export function Article({mainHeader, secondHeader, articleImg, articlePost}) {
-    return (<div>
-        <h1>{mainHeader}</h1>
-        <img src={articleImg}/>
-        <h2>{secondHeader}</h2>
-        <p>{articlePost}</p>
-        <a href="#">Discover more</a>
-    </div>);
+export default ArticleBlock;
+
+ArticleBlock.defaultProps = {
+    articles: []
+}
+
+function ArticleBlock({title, articles}) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <section>
+                {articles.map(article => (<section className={flexRow}>
+                    <img src={article.image}/>
+                    <h1>{article.title}</h1>
+                    <p>{article.post}</p>
+                    <a href="#">Discover more</a>
+                </section>))}
+            </section>
+        </div>
+    );
 }
