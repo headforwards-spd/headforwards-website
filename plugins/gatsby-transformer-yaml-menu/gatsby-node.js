d12fde8d24af18f97d13c185f23a2cae54202551
Andy Weir <andy.weir@headforwards.com>
2019-10-14 18:20:29 +0100

    .

---
+++ plugins/gatsby-transformer-yaml-menu/gatsby-node.js
@@ -0,0 +1,30 @@
+exports.onCreateNode = ({ node, getNodes, actions }) => {
+
+    if (node.internal.type === `DataYaml`) {
+
+        const {createNodeField} = actions;
+
+        const nodes = getNodes() || [];
+        const pages = nodes.filter(({ internal }) => {
+            const { type } = internal;
+            return type === 'MarkdownRemark';
+        });
+
+        const {items=[]} = node;
+
+        const menu = items.map(transformChildren);
+
+        return createNodeField({node, name:'menu', value: menu});
+
+        function transformChildren({label, link, children: _children}) {
+
+            const {fields} = pages.find(({frontmatter}) => frontmatter.uuid === link);
+            const {slug} = fields || {};
+            const url = slug || link;
+            const children = !!_children ? _children.map(transformChildren) : _children;
+
+            return {label, url, children};
+        }
+    }
+};
+
