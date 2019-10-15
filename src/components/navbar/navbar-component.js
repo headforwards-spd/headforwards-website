261885d9fa3c2e076c77ccd3f2b44f39ab934234
Emma <emma.cheney@headforwards.com>
2019-10-15 14:34:21 +0100

    navbar

--- src/components/navbar/navbar-component.js
+++ src/components/navbar/navbar-component.js
@@ -1,7 +1,8 @@
 import PropTypes          from 'prop-types';
 import React, {Component} from 'react';
-import { Link } from "gatsby"
+import {Link}             from 'gatsby';
 import Image              from '../image/image-component';
+import {flexRow} from './navbar.scss';
 
 export default class Navbar extends Component {
     static propTypes = {
@@ -24,7 +25,7 @@ export default class Navbar extends Component {
     buildMenu = (item, key) => {
         return (
             <li key={key}>
-                <Link to={item.url}>{item.label}</Link>
+                <Link className="nav-links" to={item.url}>{item.label}</Link>
                 {!!item.children && <ul>
                     {item.children.map(this.buildMenu)}
                 </ul>}
@@ -34,22 +35,22 @@ export default class Navbar extends Component {
 
     render() {
         const {menuClick} = this;
-        const {menu}     = this.props;
+        const {menu}      = this.props;
         const active      = this.state.isOpen ? 'is-active' : '';
         return (
-            <nav className="navbar">
-                <a href="/" className="logo">
-                    <Image image="/img/headforwards-full-logo-black.png" alt="Headforwards logo"/>
-                </a>
-                <ul>
-                    {menu.map(this.buildMenu)}
-                    <li onClick={menuClick}>
-                        <button className={`${active} hamburger hamburger--slider`} type="button">
+            <nav className={`${active} navbar`}>
+                <div className="flexRow">
+                    <a href="/" className="logo">
+                        <Image image="/img/headforwards-full-logo-black.png" alt="Headforwards logo"/>
+                    </a>
+                    <button onClick={menuClick} className={`${active} hamburger hamburger--slider`} type="button">
                           <span className="hamburger-box">
                             <span className="hamburger-inner"></span>
                           </span>
-                        </button>
-                    </li>
+                    </button>
+                </div>
+                <ul className={`${active} main-nav`} id="js-menu">
+                    {menu.map(this.buildMenu)}
                 </ul>
             </nav>
         );
