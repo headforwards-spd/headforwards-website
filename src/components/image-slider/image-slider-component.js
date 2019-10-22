17af11c59b390fae7fb473f49aceb52786069e80
Emma <emma.cheney@headforwards.com>
2019-10-22 09:55:37 +0100

    image removal

--- src/components/image-slider/image-slider-component.js
+++ src/components/image-slider/image-slider-component.js
@@ -4,49 +4,5 @@ import Image  from '../image/image-component'
 
 
 export function ImageSlider() {
-    const [index, setIndex] = useState(0);
-    const { allFile } = useStaticQuery(
-        graphql`
-      query {
-        allFile(
-          sort: { fields: name, order: DESC }
-          filter: { relativeDirectory: { eq: "slides" } }
-        ) {
-          edges {
-            node {
-              id
-              name
-              childImageSharp {
-                fluid(maxWidth: 600) {
-                  ...GatsbyImageSharpFluid_withWebp_tracedSVG
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-    );
-    //Minus 1 for array offset from 0
-    const length = allFile.edges.length - 1;
-    const handleNext = () =>
-        index === length ? setIndex(0) : setIndex(index + 1);
-    const handlePrevious = () =>
-        index === 0 ? setIndex(length) : setIndex(index - 1);
-    const { node } = allFile.edges[index];
-    return (
-        <div>
-            <div>
-                <Image
-                    fluid={node.childImageSharp.fluid}
-                    key={node.id}
-                    alt={node.name.replace(/-/g, ' ').substring(2)}
-                />
-            </div>
-            <div>
-                <button onClick={() => handlePrevious()}>Previous</button>
-                <button onClick={() => handleNext()}>Next</button>
-            </div>
-        </div>
-    )
+
 }
