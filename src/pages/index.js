9a38e45f3fbaaaa344a95607786227e94fa3d848
Andy Weir <andy.weir@headforwards.com>
2019-11-01 17:20:38 +0000

    header tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Layout from '../components/page-layout/layout';
 
 export default () => {
-    return (
-        <Layout>
-            <h1>Home Page</h1>
-        </Layout>
-    );
+
+    const props = {
+        title: 'Home Page',
+    };
+
+    return <Layout {...props} />;
 };
