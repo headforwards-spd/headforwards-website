28e5e3ef3c7268cd25df70a767d5383b15f2fb94
Andy Weir <andy.weir@headforwards.com>
2019-11-03 12:41:52 +0000

    menu tweaks

--- src/components/page-layout/navbar/menu-item/menu-item.component.js
+++ src/components/page-layout/navbar/menu-item/menu-item.component.js
@@ -1,9 +1,9 @@
-import { faMinus, faPlus }   from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon }   from '@fortawesome/react-fontawesome'
-import React, { Component }  from 'react';
-import Link                  from '../../../link/link.component';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { Component } from 'react';
+import Link from '../../../link/link.component';
 import { menuItemPropTypes } from './menu-item.prop-type';
-import styles                from '../navbar.module.scss';
+import styles from '../navbar.module.scss';
 
 export default class MenuItem extends Component {
     static propTypes = menuItemPropTypes;
@@ -13,12 +13,11 @@ export default class MenuItem extends Component {
     };
 
     toggleMenu(event) {
-
         event.preventDefault();
         event.stopPropagation();
 
-        const {isOpen } = this.state;
-        this.setState({isOpen: !isOpen});
+        const { isOpen } = this.state;
+        this.setState({ isOpen: !isOpen });
 
         return false;
     }
@@ -26,7 +25,7 @@ export default class MenuItem extends Component {
     render() {
         const { link, linkText, children } = this.props;
         const { isOpen } = this.state;
-        const {toggleMenu} = this;
+        const { toggleMenu } = this;
         const hasChildren = !!children;
 
         const openClass = isOpen ? styles.isOpen : '';
@@ -36,7 +35,13 @@ export default class MenuItem extends Component {
             <li className={`${childrenClass} ${openClass}`}>
                 <Link to={link} activeStyle={{ color: '#ffb800' }}>
                     {linkText}
-                    {hasChildren && <button type='button' onClick={toggleMenu.bind(this)}>{isOpen && <FontAwesomeIcon icon={faMinus} fixedWidth /> || <FontAwesomeIcon icon={faPlus} fixedWidth />}</button>}
+                    {hasChildren && (
+                        <button type="button" onClick={toggleMenu.bind(this)}>
+                            {(isOpen && <FontAwesomeIcon icon={faMinus} fixedWidth />) || (
+                                <FontAwesomeIcon icon={faPlus} fixedWidth />
+                            )}
+                        </button>
+                    )}
                 </Link>
                 {!!hasChildren && (
                     <section>
