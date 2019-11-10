b3e2f77203ea79905c047c9040776f6487a00f3d
Andy Weir <andy.weir@headforwards.com>
2019-11-10 19:22:01 +0000

    debug manifest

--- gatsby/gatsby-plugin-manifest.js
+++ gatsby/gatsby-plugin-manifest.js
@@ -9,7 +9,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'minimal-ui',
-        icon: 'static/images/icon.black.png', // This path is relative to the root of the site.
+        icon: 'static/images/icon.png', // This path is relative to the root of the site.
         icon_options: {
             // For all the options available, please see:
             // https://developer.mozilla.org/en-US/docs/Web/Manifest
