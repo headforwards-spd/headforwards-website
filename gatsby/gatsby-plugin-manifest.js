744e82f801c1a59abee4cc153263aa1f2b1839c8
Andy Weir <andy.weir@headforwards.com>
2019-11-10 18:47:35 +0000

    debug manifest

--- gatsby/gatsby-plugin-manifest.js
+++ gatsby/gatsby-plugin-manifest.js
@@ -8,14 +8,14 @@ module.exports = {
         theme_color: '#FFB800',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone',
+        display: 'minimal-ui',
         icon: 'static/images/icon.black.png', // This path is relative to the root of the site.
-        // icon_options: {
-        //     // For all the options available, please see:
-        //     // https://developer.mozilla.org/en-US/docs/Web/Manifest
-        //     // https://w3c.github.io/manifest/#purpose-member
-        //     purpose: `maskable`,
-        // },
+        icon_options: {
+            // For all the options available, please see:
+            // https://developer.mozilla.org/en-US/docs/Web/Manifest
+            // https://w3c.github.io/manifest/#purpose-member
+            purpose: `any maskable`,
+        },
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
