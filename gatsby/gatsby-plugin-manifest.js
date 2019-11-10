d4c87145c506b3b1da9fec3dcc36dade55db191c
Andy Weir <andy.weir@headforwards.com>
2019-11-10 15:44:51 +0000

    .

--- gatsby/gatsby-plugin-manifest.js
+++ gatsby/gatsby-plugin-manifest.js
@@ -8,7 +8,7 @@ module.exports = {
         theme_color: '#FFB800',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'minimal-ui',
+        display: 'standalone',
         icon: 'static/images/icon.black.png', // This path is relative to the root of the site.
         icon_options: {
             // For all the options available, please see:
