17e7fc84bc807c1a6ab34182d055893510764853
Andy Weir <andy.weir@headforwards.com>
2019-11-10 15:29:26 +0000

    manifest tweaks

--- gatsby/gatsby-plugin-manifest.js
+++ gatsby/gatsby-plugin-manifest.js
@@ -4,12 +4,18 @@ module.exports = {
         name: 'Headforwards',
         short_name: 'Headforwards',
         start_url: '/',
-        background_color: '#141122',
+        background_color: '#FFFFFF',
         theme_color: '#FFB800',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone',
-        icon: 'static/images/icon.png', // This path is relative to the root of the site.
+        display: 'minimal-ui',
+        icon: 'static/images/icon.black.png', // This path is relative to the root of the site.
+        icon_options: {
+            // For all the options available, please see:
+            // https://developer.mozilla.org/en-US/docs/Web/Manifest
+            // https://w3c.github.io/manifest/#purpose-member
+            purpose: `maskable`,
+        },
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,14 @@
-import React  from 'react';
-import Layout from '../components/page-layout/layout'
+import React from 'react';
+import Layout from '../components/page-layout/layout';
 
 export default () => {
     const props = {
         title: 'Not Found',
     };
 
-    return (<Layout {...props}>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>);
+    return (
+        <Layout {...props}>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </Layout>
+    );
 };
