87e80717d6b3bdccf3a204ce7fb9273d150f968d
Andy Weir <andy.weir@headforwards.com>
2019-11-12 22:07:41 +0000

    .

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,16 @@
 import CMS from 'netlify-cms-app';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
-import styles      from '!css-loader!sass-loader!../scss/main.scss';
-import UuidControl from './widgets/uuid-control'
+import styles from '!css-loader!sass-loader!../scss/main.scss';
+import UuidControl from './widgets/uuid-control';
 
 import DesignPagePreview from './preview-templates/design-page';
 
 CMS.registerPreviewStyle(styles.toString(), { raw: true });
 
 CMS.registerWidget({
-                           name: 'uuid',
-                           controlComponent: UuidControl,
-                       });
+    name: 'uuid',
+    controlComponent: UuidControl,
+});
 
 CMS.registerPreviewTemplate('design-pages', DesignPagePreview);
