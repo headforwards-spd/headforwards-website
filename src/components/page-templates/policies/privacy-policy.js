8e5e302d95d8453287a66b8e123eac1b49328c07
Emma <emma.cheney@headforwards.com>
2019-11-19 15:51:50 +0000

    privacy policy

---
+++ src/components/page-templates/policies/privacy-policy.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { PrivacyPolicyPropType } from '../../page-layout/pivacy-policy.prop-type';
+
+export default PrivacyPolicy;
+
+// PrivacyPolicy.propTypes = {
+//     privacyPolicy: PrivacyPolicyPropType.isRequired,
+// };
+
+function PrivacyPolicy() {
+    // const { personalData } = privacyPolicy;
+    return <div>Testing</div>;
+}
