519eb35bd2e7696cdb5418cfa667654196e9e23c
Andy Weir <andy.weir@headforwards.com>
2020-01-24 12:38:50 +0000

    Fix CMS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const onServiceWorkerUpdateReady = () => {
-    // eslint-disable-next-line no-alert
-    const answer = window.confirm(`This application has been updated.\nReload to display the latest version?`);
-    if (answer === true) {
-        window.location.reload();
-    }
-};
+export const onServiceWorkerUpdateReady = () => window.location.reload(true);
