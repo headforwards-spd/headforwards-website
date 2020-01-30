b963199959d994f69aefa75cc6d0f9d6b5e4b59f
Andy Weir <andy.weir@headforwards.com>
2020-01-30 17:09:31 +0000

    sticky nav

--- src/components/page-layout/header/header.component.js
+++ src/components/page-layout/header/header.component.js
@@ -1,6 +1,6 @@
 import parseHtml from 'html-react-parser';
 import { arrayOf, bool, string } from 'prop-types';
-import React from 'react';
+import React, { Component }      from 'react';
 
 import { CompanyInfoPropType } from '../company-info.prop-type';
 import Image, { ImageSrcPropType } from '../image/image.component';
@@ -8,38 +8,90 @@ import { MenuItemPropType } from '../navbar/menu-item/menu-item.prop-type';
 import Navbar from '../navbar/navbar.component';
 import styles from './header.module.scss';
 
-export default Header;
+// Header.propTypes = {
+//     isHomePage: bool,
+//     title: string.isRequired,
+//     subtitle: string,
+//     image: ImageSrcPropType,
+//     menu: arrayOf(MenuItemPropType).isRequired,
+//     companyInfo: CompanyInfoPropType.isRequired,
+// };
+// Header.defaultProps = {
+//     isHomePage: false,
+//     subtitle: null,
+//     image: null,
+// };
+export default class Header extends Component {
 
-Header.propTypes = {
-    isHomePage: bool,
-    title: string.isRequired,
-    subtitle: string,
-    image: ImageSrcPropType,
-    menu: arrayOf(MenuItemPropType).isRequired,
-    companyInfo: CompanyInfoPropType.isRequired,
-};
-Header.defaultProps = {
-    isHomePage: false,
-    subtitle: null,
-    image: null,
-};
-function Header({ isHomePage, title, subtitle, image, menu, companyInfo }) {
-    const { publicURL } = image || {};
-    const hasBackground = !!image || !!publicURL;
-    let headerStyle = hasBackground ? styles.hasBackground : '';
+    scrollTop = 0;
+    debounceTime = 15;
+    debounceScroll = null;
 
-    isHomePage && (headerStyle += ` ${styles.isHomePage}`);
+    state = {
+        isScrollingUp: false,
+        isScrollingDown: false,
+    };
 
-    const titleStyle = subtitle ? styles.hasSubTitle : '';
+    componentDidMount() {
 
-    return (
-        <header className={`${styles.header} ${headerStyle}`}>
-            <Navbar {...{ menu, companyInfo, hasBackground }} />
-            <section className={titleStyle}>
-                <h1>{parseHtml(title)}</h1>
-                {!!subtitle && <p>{subtitle}</p>}
-            </section>
-            {!!image && <Image image={image} alt={title} className={styles.image} />}
-        </header>
-    );
+        this.scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        window.addEventListener('scroll', this.handleScroll.bind(this));
+    }
+
+    handleScroll() {
+
+        const {debounceTime, debounceScroll} = this;
+
+        debounceScroll && clearTimeout(debounceScroll);
+        this.debounceScroll = setTimeout(() => {
+
+            const scrollTop     = window.pageYOffset || document.documentElement.scrollTop;
+            const isAtTop       = scrollTop < 5;
+            const isNearTop     = scrollTop < 200;
+
+            const {isScrollingUp, isScrollingDown} = this.state;
+
+            const isScrollingUpNext   = scrollTop < this.scrollTop && (!isNearTop || isScrollingUp);
+            const isScrollingDownNext = scrollTop > this.scrollTop && (!isNearTop || isScrollingDown);
+
+
+            if(isAtTop && (isScrollingUp || isScrollingDown)) {
+                this.setState({isScrollingUp: false, isScrollingDown: false});
+            } else if(isScrollingUp !== isScrollingUpNext || isScrollingDown !== isScrollingDownNext) {
+                this.setState({isScrollingUp: isScrollingUpNext, isScrollingDown: isScrollingDownNext});
+            }
+
+            this.scrollTop = scrollTop;
+        }, debounceTime);
+    }
+
+
+    render() {
+
+        const { isHomePage, title, subtitle, image, menu, companyInfo } = this.props;
+
+        const {isScrollingUp, isScrollingDown} = this.state;
+
+        const scrollingClass = isScrollingUp ? styles.isScrollingUp : isScrollingDown ? styles.isScrollingDown : '';
+
+        const { publicURL } = image || {};
+        const hasBackground = !!image || !!publicURL;
+        let headerStyle = hasBackground ? styles.hasBackground : '';
+
+        isHomePage && (headerStyle += ` ${styles.isHomePage}`);
+
+        const titleStyle = subtitle ? styles.hasSubTitle : '';
+
+        return (
+            <header className={`${styles.header} ${headerStyle} ${scrollingClass}`}>
+                <Navbar {...{ menu, companyInfo, hasBackground }} />
+                <section className={titleStyle}>
+                    <h1>{parseHtml(title)}</h1>
+                    {!!subtitle && <p>{subtitle}</p>}
+                </section>
+                {!!image && <Image image={image} alt={title} className={styles.image} />}
+            </header>
+        );
+    }
 }
--- src/components/page-layout/navbar/menu/menu.component.js
+++ src/components/page-layout/navbar/menu/menu.component.js
@@ -1,5 +1,5 @@
-import { Location } from '@reach/router';
-import React from 'react';
+import { Location }         from '@reach/router';
+import React, { Component } from 'react';
 
 import Link from '../../link/link.component';
 import Socials from '../../socials/socials.component';
@@ -8,84 +8,84 @@ import MenuItem from '../menu-item/menu-item.component';
 import styles from '../navbar.module.scss';
 import { menuPropTypes } from './menu.prop-type';
 
-export default Menu;
+// Menu.propTypes = menuPropTypes;
+// Menu.defaultProps = {
+//     hasBackground: false,
+//     activeClass: null,
+// };
+export default class Menu extends Component{
 
-Menu.propTypes = menuPropTypes;
-Menu.defaultProps = {
-    hasBackground: false,
-    activeClass: null,
-};
-function Menu({ menuClick, hasBackground, activeClass, menu, companyInfo }) {
-    const { companyName, phone } = companyInfo;
+    render() {
 
-    const backgroundImg = hasBackground ? 'with-bg' : 'without-bg';
+        const { menuClick, hasBackground, activeClass, menu, companyInfo } = this.props;
+        const { companyName, phone } = companyInfo;
+        const backgroundImg = hasBackground ? 'with-bg' : 'without-bg';
 
-    return (
-        <Location>
-            {({ location }) => (
-                <>
-                    <nav className={`${activeClass} ${styles.menu} ${backgroundImg}`}>
-                        <header>
-                            <Link to="/">{companyName}</Link>
-                            <Hamburger
-                                {...{
-                                    activeClass,
-                                    onClick: menuClick,
-                                }}
-                            />
-                        </header>
-                        <section>
-                            <ul>
-                                <MenuItem
-                                    className={styles.navHomeLink}
+        return (
+            <Location>
+                {({ location }) => (
+                    <>
+                        <nav className={`${activeClass} ${styles.menu} ${backgroundImg}`}>
+                            <header>
+                                <Link to="/">{companyName}</Link>
+                                <Hamburger
                                     {...{
-                                        id: 'nav-home-link',
-                                        showTitle: true,
-                                        location,
-                                        link: '/',
-                                        linkText: 'Home.',
-                                        children: [
-                                            {
-                                                id: 'nav-home-contact-page-link',
-                                                location,
-                                                link: '/contact',
-                                                linkText: 'Contact',
-                                            },
-                                        ],
+                                        activeClass,
+                                        onClick: menuClick,
                                     }}
                                 />
-                                {menu.map(({ id, ...item }) => (
-                                    <MenuItem key={id} {...item} {...{ location, showTitle: true }} />
-                                ))}
-                                <MenuItem
-                                    className={styles.navContactLink}
-                                    {...{
-                                        id: 'nav-contact-link',
-                                        link: '/contact',
-                                        linkText: 'Contact.',
-                                        location,
-                                    }}
-                                />
-                            </ul>
-                            <section className={styles.contactDetails}>
-                                <dl>
-                                    <dt>Call us.</dt>
-                                    <dd>{phone}</dd>
-                                </dl>
-                                <section>
-                                    <h2>Follow us.</h2>
-                                    <Socials
-                                        {...{
-                                            ...companyInfo,
-                                            activeClass,
-                                        }}
+                            </header>
+                            <section>
+                                <ul>
+                                    <MenuItem className={styles.navHomeLink}
+                                              {...{
+                                                  id:        'nav-home-link',
+                                                  showTitle: true,
+                                                  location,
+                                                  link:      '/',
+                                                  linkText:  'Home.',
+                                                  children:  [
+                                                      {
+                                                          id:       'nav-home-contact-page-link',
+                                                          location,
+                                                          link:     '/contact',
+                                                          linkText: 'Contact',
+                                                      },
+                                                  ],
+                                              }}
                                     />
+                                    {menu.map(({ id, ...item }) => (
+                                        <MenuItem key={id} {...item} {...{
+                                            location,
+                                            showTitle: true
+                                        }} />))}
+                                    <MenuItem className={styles.navContactLink}
+                                              {...{
+                                                  id:       'nav-contact-link',
+                                                  link:     '/contact',
+                                                  linkText: 'Contact.',
+                                                  location,
+                                              }}
+                                    />
+                                </ul>
+                                <section className={styles.contactDetails}>
+                                    <dl>
+                                        <dt>Call us.</dt>
+                                        <dd>{phone}</dd>
+                                    </dl>
+                                    <section>
+                                        <h2>Follow us.</h2>
+                                        <Socials
+                                            {...{
+                                                ...companyInfo,
+                                                activeClass,
+                                            }}
+                                        />
+                                    </section>
                                 </section>
                             </section>
-                        </section>
-                    </nav>
-                </>
-            )}
-        </Location>
-    );
+                        </nav>
+                    </>)}
+            </Location>);
+    }
 }
