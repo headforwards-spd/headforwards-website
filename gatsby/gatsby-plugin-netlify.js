17b77fbefda4a2c565dec87e5c0126fba2013017
Andy Weir <andy.weir@headforwards.com>
2020-02-28 10:29:57 +0000

    Auth Header

--- gatsby/gatsby-plugin-netlify.js
+++ gatsby/gatsby-plugin-netlify.js
@@ -1,4 +1,5 @@
-module.exports = {
+const sitePassword = process.env.SITE_PASSWORD || null;
+const config = {
     resolve: `gatsby-plugin-netlify`,
     options: {
         headers: {
@@ -10,3 +11,7 @@ module.exports = {
         generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
     },
 };
+
+sitePassword && (config.options.headers['/*'] = [`Basic-Auth: login:${sitePassword}`]);
+
+module.exports = config;
